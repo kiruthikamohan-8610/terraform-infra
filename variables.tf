c90427a3d52cb58c4a4fa366e82ae6c677931bff
kiruthikamohan-8610 <kiruthikamohanarangam@gmail.com>
2026-07-13 09:06:56 +0530

    Define variables for AWS and PostgreSQL configuration
    Added variables for AWS region, project name, environment, and PostgreSQL database configuration.

--- variables.tf
+++ variables.tf
@@ -1,1 +1,34 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "microservices-demo"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
+
+variable "db_name" {
+  description = "PostgreSQL database name"
+  type        = string
+}
+
+variable "db_username" {
+  description = "PostgreSQL username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "PostgreSQL password"
+  type        = string
+  sensitive   = true
+}
