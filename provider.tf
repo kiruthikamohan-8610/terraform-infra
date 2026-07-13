7272b10b79d9f5ad157a581dba90f458316128e6
kiruthikamohan-8610 <kiruthikamohanarangam@gmail.com>
2026-07-13 09:06:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
 }
